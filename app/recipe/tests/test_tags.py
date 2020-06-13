71a1e91c8e2c92af8fdf87caa93e12ffb8d7dd03
Hillary Chepsergon <hillergon@gmail.com>
2020-06-13 23:30:25 +0300

    added tags endpoint and tests

--- app/recipe/tests/test_tags.py
+++ app/recipe/tests/test_tags.py
@@ -7,8 +7,8 @@ from rest_framework.test import APIClient
 from recipe.serializer import TagSerializer
 from core.models import Tag
 
-TAGS_URL = reverse('recipe:tags-list')
-
+# TAGS_URL = reverse('recipe:tags-list')
+TAGS_URL = reverse('recipe:tag-list')
 
 class PublicTagsApiTests(TestCase):
     """test tags api that don't require auth"""
@@ -29,21 +29,21 @@ class PrivateApiTests(TestCase):
             email='hillary@baratel.com',
             password='password123')
         self.client = APIClient()
-        self.force_authenticate(self.user)
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_tags(self):
         """test retrieving tags"""
 
-        Tag.create(
+        Tag.objects.create(
             user = self.user,
             name = 'nyamas'
         )
-        Tag.create(
+        Tag.objects.create(
             user=self.user,
             name='mboga'
         )
         res = self.client.get(TAGS_URL)
-        tags = Tag.objects.all().oder_by('-name')
+        tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags,many=True)
         self.assertEqual(res.status_code,status.HTTP_200_OK)
         self.assertEqual(res.data,serializer.data)
@@ -51,7 +51,7 @@ class PrivateApiTests(TestCase):
 
     def test_tags_limited_to_user(self):
         """test tags are limited to the authenticated user"""
-        user2 = get_user_model().create_user(
+        user2 = get_user_model().objects.create_user(
             email='other@baratel.com',
             password='mypasskangogo'
         )
